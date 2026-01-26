9391d9ccfae659f2a9b46b973a2dd338c5504dee
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-01-26 12:09:25 +0000

    Add OKLab values for palette. Remove reference from palette because it has been modified enough that it no longer matches the reference.

--- website/src/constants.js
+++ website/src/constants.js
@@ -3,14 +3,13 @@ export const FRAME_TIME_MS = FRAME_TIME * 1000.0;
 
 export const CHAR_WIDTH = 16;
 
-// https://lospec.com/palette-list/shmupy-16
 export const PALETTE = [
-    '#ffffff',
-    '#636363',
-    '#cc2222',
-    '#ff8822',
-    '#ffcc33',
-    '#88cc44',
-    '#3377dd',
-    '#8e20bd',
+    '#ffffff',// 1 0 0
+    '#636363',// 0.5 0 0
+    '#cc2222',// 0.545 0.2036 27.36
+    '#e5882A',// 0.7116 0.1515 60.23
+    '#ffcc00',// 0.8652 0.1768 90.38
+    '#88cc44',// 0.7729 0.1803 132.16
+    '#3377dd',// 0.5804 0.1704 258.65
+    '#8e20bd',// 0.5012 0.2271 312.5
 ]
