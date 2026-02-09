de7ed91710b72c12f873b841f0f1deb9024a19e9
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-02-09 21:59:46 +0000

    Compression: Add PPMd code. Setup C++ to WASM compilation with test program.

--- website/src/index.js
+++ website/src/index.js
@@ -1,2 +1,12 @@
 import './style.css';
 import _ from './pwa.js'
+import CppApp from "./compiled/app.js";
+(async () => {
+  let app = await CppApp();
+
+  try {
+    app.callMain();
+  } catch (e) {
+    console.error(e.stack);
+  }
+})();
