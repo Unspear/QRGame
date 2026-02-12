76254fa1e6d360a350e581c228f1ec8eada8fd30
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-02-12 17:35:42 +0000

    Webpack: Remove deprecated hash setting

--- website/webpack.config.js
+++ website/webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[hash].css",
+      filename: "[contenthash].css",
     }),
     new HtmlWebpackPlugin({
       chunks: ["index"],
