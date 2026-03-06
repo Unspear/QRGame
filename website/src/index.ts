436104c9c3df3c00b9ee1a777c58a6298a7eed15
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-03-06 16:42:47 +0000

    Website: Minor UX polish

--- website/src/index.ts
+++ website/src/index.ts
@@ -2,11 +2,14 @@ import './page'
 import {gameToUrl} from './pack'
 import library from './library'
 
+const demoParent = document.getElementById("demos") as HTMLElement;
+
 for(const entry of library) {
+    let p = document.createElement('p');
     let a = document.createElement('a');
     a.href = gameToUrl(entry.game, "play");
     a.text = entry.title;
-    document.body.appendChild(a);
-    document.body.appendChild(document.createTextNode(" - "+entry.description));
-    document.body.appendChild(document.createElement('br'));
+    p.appendChild(a);
+    p.appendChild(document.createTextNode(" - " + entry.description));
+    demoParent.appendChild(p);
 }
