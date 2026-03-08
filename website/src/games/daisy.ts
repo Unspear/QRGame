118bd7beb8a093fbf20464371a28d13da7670bdc
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-03-08 21:14:23 +0000

    Games: Shorten Daisy Bell to one verse

--- website/src/games/daisy.ts
+++ website/src/games/daisy.ts
@@ -25,14 +25,6 @@ function frame()
 end
 
 say([[
-There is a flower within my heart, Daisy, Daisy!
-Planted one day by a glancing dart,
-Planted by Daisy Bell!
-Whether she loves me or loves me not,
-Sometimes it's hard to tell;
-Yet I am longing to share the lot
-Of beautiful Daisy Bell!
-
 Daisy, Daisy,
 Give me your answer, do!
 I'm half crazy,
@@ -41,22 +33,6 @@ It won't be a stylish marriage,
 I can't afford a carriage,
 But you'll look sweet upon the seat
 Of a bicycle built for two!
-
-We will go tandem as man and wife, Daisy, Daisy!
-Pedaling away down the road of life,
-I and my Daisy Bell!
-When the road's dark we can both despise
-Policeman and lamps as well;
-There are bright lights in the dazzling eyes
-Of beautiful Daisy Bell!
-
-I will stand by you in wheel or woe, Daisy, Daisy!
-You'll be the bell which I'll ring you know!
-Sweet little Daisy Bell!
-You'll take the lead in each trip we take,
-Then if I don't do well;
-I will permit you to use the brake,
-My beautiful Daisy Bell!
 ]])`
 export default function (): Game {
     return new Game(script);
