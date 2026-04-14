b16344248f205e050fc736b1f004199765f6645b
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-04-14 17:56:33 +0100

    Platformer: Detect floor. Show health and timer. Use screen entities.

--- website/src/games/platformer.ts
+++ website/src/games/platformer.ts
@@ -20,27 +20,38 @@ player.frame = function()
     endGame("🎇You Win🎇")
   end
 end
-local left = createEntity(" <- ", 8, 32, 256-32)
+local left = createScreenEntity(" <- ", 8, 32, 234)
 left.input.enabled = true
 left.input.dim = {x = 64, y = 48}
 left.input.key = "arrowleft"
 left.input.hold = function()
   player.physics.vel.x = -1
 end
-local right = createEntity(" -> ", 8, 96, 256-32)
+local right = createScreenEntity(" -> ", 8, 96, 234)
 right.input.enabled = true
 right.input.dim = {x = 64, y = 48}
 right.input.key = "arrowright"
 right.input.hold = function() 
   player.physics.vel.x = 1
 end
-local jump = createEntity("JUMP", 8, 160, 256-32)
+local jump = createScreenEntity("JUMP", 8, 160, 234)
 jump.input.enabled = true
 jump.input.dim = {x = 64, y = 48}
 jump.input.key = "arrowup"
 jump.input.press = function()
-  player.physics.vel.y = -3.2
-end`
+  if player.physics.onFloor then
+    player.physics.vel.y = -3.2
+  end
+end
+local timer = createScreenEntity("", 4, 192-6, 6)
+timer.sprite.pivot = {x=1,y=-1}
+local seconds = 0
+timer.frame = function()
+  seconds = seconds + FRAME_TIME
+  timer.sprite.char = tostring(math.floor(seconds*10.0))
+end
+local health = createScreenEntity("🖤🖤🖤", 2, 6, 6)
+health.sprite.pivot = {x=-1,y=-1}`
 let tiles: string = '{"tileMap":{"dim":{"w":2,"h":8},"count":12,"tileData":[{"codePoint":[32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32],"color":[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14]},{"codePoint":[32,32,32,32,32,32,32,32,35,35,61,61,61,61,61,61],"color":[14,14,14,14,14,14,14,14,10,10,11,11,11,11,11,11]},{"codePoint":[32,32,32,32,35,35,61,61,61,61,61,61,61,61,61,61],"color":[14,14,14,14,10,10,11,11,11,11,11,11,11,11,11,11]},{"codePoint":[35,35,61,61,61,61,61,61,61,61,61,61,61,61,61,61],"color":[10,10,11,11,11,11,11,11,11,11,11,11,11,11,11,11]},{"codePoint":[32,32,35,35,32,32,32,32,35,35,61,61,61,61,61,61],"color":[14,14,9,9,14,14,14,14,10,10,11,11,11,11,11,11]},{"codePoint":[32,32,32,32,32,32,32,32,32,32,35,35,32,32,32,32],"color":[14,14,14,14,14,14,14,14,14,14,9,9,14,14,14,14]},{"codePoint":[72,72,72,72,72,72,72,72,72,72,72,72,72,72,72,72],"color":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},{"codePoint":[32,32,32,32,32,32,32,32,128638,128638,61,61,61,61,61,61],"color":[14,14,14,14,14,14,14,14,8,8,11,11,11,11,11,11]},{"codePoint":[32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32],"color":[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14]},{"codePoint":[32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32],"color":[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14]},{"codePoint":[32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32],"color":[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14]},{"codePoint":[32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32],"color":[14,14,14,14,14,14,14,14,14,14,14,14,14,14,14,14]}]},"patchMap":{"dim":{"w":72,"h":2},"tileData":{"patchId":[6,0,0,0,0,0,5,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,6,6,1,1,1,4,3,3,3,3,4,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,7,1,6],"transform":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]}}}'
 export default function (): Game {
     let parsed = JSON.parse(tiles);
