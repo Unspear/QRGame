c2d568f7534fc98716584fa5fed8e1854f56dfa8
CyborgCabbage <finlaylaidlaw9@gmail.com>
2026-04-24 18:52:19 +0100

    PWA: Fix new games not working offline

--- website/webpack.common.js
+++ website/webpack.common.js
@@ -95,7 +95,8 @@ module.exports = {
        // these options encourage the ServiceWorkers to get in there fast
        // and not allow any straggling "old" SWs to hang around
        clientsClaim: true,
-       skipWaiting: true
+       skipWaiting: true,
+       ignoreURLParametersMatching: [/.*/]
     })
   ]
 };
